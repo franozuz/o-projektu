80418fe7c91ec546eb02f8db3eb7307751cf7cf8
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-05-20 20:04:14 +0200

    Grid vytvoreny

--- Julia/index.js
+++ Julia/index.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log('funguju');
+
+const divLength = document.getElementsByTagName('div').length;
+
+let i;
+const gridColors = () => {
+  for (i = 0; i < divLength; i++) {
+    const r = Math.floor(Math.random() * 5);
+    const b = Math.floor(Math.random() * 5);
+    const y = Math.floor(Math.random() * 5);
+
+    const colors = 'rby(' + r + ',' + b + ',' + y + ')';
+    divLength[i].style.backgroundColor = colors;
+  }
+};
+
+const btnElm = document.querySelector('button');
+btnElm.addEventListener('click', gridColors);
